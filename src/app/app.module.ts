291c2e66a4b23be5eed979d75528b6ec7817d04d
Mohammad Askari <32340808+pyrrhicman@users.noreply.github.com>
2019-02-25 23:25:18 +0200

    m,,,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LaptopListComponent } from './laptop-list/laptop-list.component';
 import { MyHeaderComponent } from './my-header/my-header.component';
 import { LaptopComponent } from './laptop-list/laptop/laptop.component';
 import { LaptopAdderComponent } from './laptop-adder/laptop-adder.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckbox, MatCheckboxModule} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +19,7 @@ import {MatButtonModule, MatCheckbox, MatCheckboxModule} from '@angular/material
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatButtonModule, MatCheckboxModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
